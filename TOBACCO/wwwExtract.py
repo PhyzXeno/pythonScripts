0dfed8278c166e3a44a50e5f3089b519e3cf45cb
unknown <>
2016-08-17 21:12:54 +0800

    add-tobacco

--- TOBACCO/wwwExtract.py
+++ TOBACCO/wwwExtract.py
@@ -3,12 +3,12 @@
 import re
 #file=open('./weihai1.html','r')
 file2=open('./wwwList.txt','a+')
-for time in range(1,4):
+for time in range(1,6):
     time_str="%d" %time
-    name=time_str + '.txt'
+    name=time_str + '.htm'
     file=open(name,'r',encoding='utf-8')
     for line in file:
-        if "zjk" in line:
+        if "tobacco" in line:
             file2.write(line)
         else:
             pass
