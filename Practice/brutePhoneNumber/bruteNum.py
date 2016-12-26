678512787e21a1e48d5fbd0153b218fe7102935e
unknown <>
2016-12-26 22:35:31 +0800

    add-brute

--- Practice/brutePhoneNumber/bruteNum.py
+++ Practice/brutePhoneNumber/bruteNum.py
@@ -5,5 +5,7 @@ file_i = open(base_path + '\\' + '4tailNumber.txt','r')
 file_o = open(base_path + '\\' + '1340031.txt','w')
 
 for line in file_i:
-    if line:
         file_o.writelines('1340031' + line)
+
+file_i.close()
+file_o.close()
