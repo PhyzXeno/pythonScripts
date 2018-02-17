107e368e4d01c834321e327fb022d214fa916629
PhyzXeno <630172407@qq.com>
2018-02-18 05:57:33 +0800

    can work only with cookie

--- Practice/Bilibili/test.py
+++ Practice/Bilibili/test.py
@@ -1,7 +1,11 @@
 import requests
 
 id = "19618640"
-url = "http://api.bilibili.com/view?type=jsonp&appkey=8e9fc618fbd41e28&id=" + id + "&page=1&callback=fn"
-url2 = "http://api.bilibili.com//playurl?aid=" + id + "&page=${ value }&platform=html5&quality=1&vtype=mp4&type=jsonp&callback=fn&token=d3bd9275f0f2cda07f2406740db06c5d"
-r = requests.get(url2)
-print(r.text.encode("utf-8").decode("unicode_escape"))
+headers = {
+    "User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:57.0) Gecko/20100101 Firefox/57.0",
+    "Cookie" : "sid=59bo484f; buvid3=A13536B3-4171-49FA-8A13-35A1A729C68A14072infoc; LIVE_BUVID=AUTO6715179853023347; fts=1517985341; finger=dd823f11; pgv_pvi=1014652928; rpdid=kmoxkplkoldosomlqskww"
+    }
+url = "http://api.bilibili.com//playurl?aid=19618640&page=1&platform=html5&quality=1&vtype=mp4&type=jsonp&callback=fn&token=d3bd9275f0f2cda07f2406740db06c5d&callback=mduijsonp_1518883361692_9"
+r = requests.get(url,headers = headers)
+print(r.text.encode('utf-8').decode('unicode_escape'))
+# print(r.cookies)
